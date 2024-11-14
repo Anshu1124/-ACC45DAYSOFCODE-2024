f07ec078374c8a37707ad45e438e25e8b909c5a3
Anshu1124 <anshika1124agarwal@gmail.com>
2024-11-14 21:31:06 +0530

    Add files via upload

---
+++ DAY-43 PASSORFAIL (1).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+int t;
+scanf("%d",&t);
+while(t--)
+{
+    
+int n,x,p;
+scanf("%d%d%d",&n,&x,&p);
+
+    
+   int a=n*3;
+	    int b=x*3-(n-x);
+	    if(b>=p){
+	        printf("PASS\n");
+	    } else {
+	        printf("FAIL\n");
+	    }  
+    
+    
+    
+    
+    
+}
+
+}
+
